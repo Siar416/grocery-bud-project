6357232ac384d1ae984cfce504fa78455b947b2e
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-22 13:05:58 -0400

    added clear list func to delete all items

--- setup/src/App.js
+++ setup/src/App.js
@@ -21,7 +21,7 @@ function App() {
     } else if (name && isEditing) {
       // deal with edit
     } else {
-      // show alert
+      showAlert(true, "success", "item added to list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
@@ -34,6 +34,11 @@ function App() {
       type,
       msg,
     });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
   };
 
   return (
@@ -57,7 +62,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear item</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear item
+          </button>
         </div>
       )}
     </section>
