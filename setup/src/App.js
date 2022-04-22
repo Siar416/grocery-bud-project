e6574b3b7cccbb91ff2f296f9e04bcbbc3f34e60
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-22 12:54:57 -0400

    added func show alert to display msg if form is submitted empty

--- setup/src/App.js
+++ setup/src/App.js
@@ -8,15 +8,16 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
-    show: true,
-    msg: "hello world",
-    type: "success",
+    show: false,
+    msg: "",
+    type: "",
   });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
       // display alert
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // deal with edit
     } else {
@@ -27,10 +28,18 @@ function App() {
     }
   };
 
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({
+      show,
+      type,
+      msg,
+    });
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
