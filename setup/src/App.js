67de93c720b24275b12d3faed2497c40fabcf72f
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-22 13:54:37 -0400

    added local storage func

--- setup/src/App.js
+++ setup/src/App.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
+const getLocalStorage = () => {
+  const localStorageItems = localStorage.getItem("list");
+  if (localStorageItems) {
+    return JSON.parse(localStorageItems);
+  }
+};
+
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage() || []);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -63,6 +70,10 @@ function App() {
     setEditID(id);
     setName(specificItem.title);
   };
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="section-center">
